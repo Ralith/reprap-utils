c21ceb4db173daaa1b42431341e12e2aca0e3fd7
Ralith <ralith@gmail.com>
2009-07-24 22:15:52 -0700

    Renamed rru-control to rru-gcode-gen to reflect functionality.

---
+++ gcode-gen/CMakeFiles/rru-control.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/rru-control.dir/control.o"
+  "rru-control.pdb"
+  "rru-control"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/rru-control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
