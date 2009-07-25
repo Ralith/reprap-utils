8f3db7bd319ae14342d87315e8788d3551437086
Ralith <ralith@gmail.com>
2009-07-24 17:34:52 -0700

    Framework for command option processing, gcode linked list convenience funcs

--- control/control.c
+++ control/control.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
 #include <popt.h>
 
 #include "../common/serial.h"
@@ -10,20 +13,53 @@
 
 #define DEFAULT_SPEED 19200
 
-typedef struct _gcode_cmd
+typedef struct gcode_cmd
 {
 	char *command;
-	struct _gcode_cmd *next;
+	struct gcode_cmd *next;
 } gcode_cmd;
 
-int main(int argc, const char** argv) 
+gcode_cmd* gcode(char *cmd, int argc, char **argv) 
+{
+	gcode_cmd *ret = malloc(sizeof(gcode_cmd));
+	size_t len = 0;
+	
+	len += strlen(cmd);
+
+	int i;
+	for(i = 0; i < argc; i++) {
+		len += strlen(argv[i]);
+	}
+
+	ret->command = calloc(len+1, sizeof(char));
+	strcpy(ret->command, cmd);
+
+	for(i = 0; i < argc; i++) {
+		strcat(ret->command, argv[i]);
+	}
+	ret->next = NULL;
+	
+	return ret;
+}
+
+void gcode_append(gcode_cmd **tail, gcode_cmd *new) 
+{
+	assert((*tail)->next == NULL);
+	(*tail)->next = new;
+	*tail = new;
+}
+
+int main(int argc, const char **argv) 
 {
 	init_sig_handling();
+
+	gcode_cmd *head, *tail;
+	head = gcode("G21", 0, NULL);
+	tail = head;
 
 	/* Get options */
 	long speed = DEFAULT_SPEED;
 	char *devpath = NULL;
-	gcode_cmd head, *tail;
 	int verbose = 0;
 	{
 		poptContext ctx;
@@ -32,31 +68,31 @@ int main(int argc, const char** argv)
 			{"linespeed", 'l', POPT_ARG_LONG, &speed, 0,
 			 "Serial linespeed (defaults to " STR(DEFAULT_SPEED) ".", "<speed>"},
 
-			{"speed", 's', POPT_ARG_INT, NULL, 0,
+			{"speed", 's', POPT_ARG_INT, NULL, 's',
 			 "Set movement speed.", "speed"},
-			{"rapid", 'p', POPT_ARG_STRING, NULL, 0,
+			{"rapid", 'p', POPT_ARG_STRING, NULL, 'p',
 			 "Rapid positioning (G0).", "[x]:[y]:[z]"},
-			{"linear", 'l', POPT_ARG_STRING, NULL, 0,
+			{"linear", 'l', POPT_ARG_STRING, NULL, 'l',
 			 "Linear move (G1).", "<[x]:[y]:[z]>"},
-			{"dwell", 'd', POPT_ARG_INT, NULL, 0,
+			{"dwell", 'd', POPT_ARG_INT, NULL, 'd',
 			 "Dwell for <time> seconds (G4).", "time"},
-			{"inches", 'i', POPT_ARG_NONE, NULL, 0,
+			{"inches", 'i', POPT_ARG_NONE, NULL, 'i',
 			 "Set units to inches (G20).", NULL},
-			{"milimeters", 'm', POPT_ARG_NONE, NULL, 0,
+			{"milimeters", 'm', POPT_ARG_NONE, NULL, 'm',
 			 "Set units to milimeters (default) (G21).", NULL},
-			{"absolute", 'a', POPT_ARG_NONE, NULL, 0,
+			{"absolute", 'a', POPT_ARG_NONE, NULL, 'a',
 			 "Use absolute coordinates (default) (G90).", NULL},
-			{"relative", 'r', POPT_ARG_NONE, NULL, 0,
+			{"relative", 'r', POPT_ARG_NONE, NULL, 'r',
 			 "Use relative/incremental coordinates (G91).", NULL},
 			
-			{"extrude", 'e', POPT_ARG_STRING, NULL, 0,
+			{"extrude", 'e', POPT_ARG_STRING, NULL, 'e',
 			 "Set extruder motor state (M101/M103).", "on|off|reverse"},
-			{"temp", 't', POPT_ARG_INT, NULL, 0,
+			{"temp", 't', POPT_ARG_INT, NULL, 't',
 			 "Set extrusion temperature in Celsius (M104).", "temperature"},
-			{"flowrate", 'f', POPT_ARG_INT, NULL, 0,
+			{"flowrate", 'f', POPT_ARG_INT, NULL, 'f',
 			 "Sets extrusion motor speed (M108).", "speed"},
 
-			{"zero", 'z', POPT_ARG_STRING, NULL, 0,
+			{"zero", 'z', POPT_ARG_STRING, NULL, 'z',
 			 "Zeroes the axes named in the argument.", "[x][y][z]"},
 			POPT_AUTOHELP
 			{NULL, 0, 0, NULL, 0}
@@ -66,6 +102,22 @@ int main(int argc, const char** argv)
 		poptSetOtherOptionHelp(ctx, "<serial device>");
 
 		if (argc < 2) {
+			poptPrintUsage(ctx, stderr, 0);
+			exit(EXIT_FAILURE);
+		}
+
+		/* Process args */
+		int rc;
+		while((rc = poptGetNextOpt(ctx)) > 0) {
+			switch(rc) {
+			default:
+				break;
+			}
+		}
+		/* Anything remaining is the dev path. */
+		devpath = poptGetArg(ctx);
+
+		if((devpath == NULL) || (poptPeekArg(ctx) != NULL)) {
 			poptPrintUsage(ctx, stderr, 0);
 			exit(EXIT_FAILURE);
 		}
