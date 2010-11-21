d5273eee2231920f6350d6b695b9595fb08bc8fe
Benjamin Saunders <besaunde@sfu.ca>
2010-11-20 16:57:14 -0800

    Style fix thank god

--- gcview/gcview.c
+++ gcview/gcview.c
@@ -34,27 +34,27 @@ struct {
 
 /* Draw the current state of affairs */
 void draw(void) {
-  glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   /* Move Left 1.5 Units And Into The Screen 6.0 */
   glLoadIdentity();
-  glTranslatef( -1.5f, 0.0f, -6.0f );
+  glTranslatef(-1.5f, 0.0f, -6.0f);
 
-  glBegin( GL_TRIANGLES );            /* Drawing Using Triangles */
-  glVertex3f(  0.0f,  1.0f, 0.0f ); /* Top */
-  glVertex3f( -1.0f, -1.0f, 0.0f ); /* Bottom Left */
-  glVertex3f(  1.0f, -1.0f, 0.0f ); /* Bottom Right */
-  glEnd( );                           /* Finished Drawing The Triangle */
+  glBegin(GL_TRIANGLES);            /* Drawing Using Triangles */
+  glVertex3f( 0.0f,  1.0f, 0.0f); /* Top */
+  glVertex3f(-1.0f, -1.0f, 0.0f); /* Bottom Left */
+  glVertex3f( 1.0f, -1.0f, 0.0f); /* Bottom Right */
+  glEnd();                           /* Finished Drawing The Triangle */
 
   /* Move Right 3 Units */
-  glTranslatef( 3.0f, 0.0f, 0.0f );
+  glTranslatef(3.0f, 0.0f, 0.0f);
 
-  glBegin( GL_QUADS );                /* Draw A Quad */
-  glVertex3f( -1.0f,  1.0f, 0.0f ); /* Top Left */
-  glVertex3f(  1.0f,  1.0f, 0.0f ); /* Top Right */
-  glVertex3f(  1.0f, -1.0f, 0.0f ); /* Bottom Right */
-  glVertex3f( -1.0f, -1.0f, 0.0f ); /* Bottom Left */
-  glEnd( );                           /* Done Drawing The Quad */
+  glBegin(GL_QUADS);                /* Draw A Quad */
+  glVertex3f(-1.0f,  1.0f, 0.0f); /* Top Left */
+  glVertex3f( 1.0f,  1.0f, 0.0f); /* Top Right */
+  glVertex3f( 1.0f, -1.0f, 0.0f); /* Bottom Right */
+  glVertex3f(-1.0f, -1.0f, 0.0f); /* Bottom Left */
+  glEnd();                           /* Done Drawing The Quad */
 
   glutSwapBuffers();
 
